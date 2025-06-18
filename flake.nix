db9500fe456a6f0485c35d46f066cb56f3228303
Tony Merlin <code@what-the-functor.com>
2025-06-18 21:17:15 +0200

    Set up dev environment

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "flake-pin";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      ghc = "ghc984"; # 9.8.4
+
+      systems = [
+        "aarch64-darwin"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "x86_64-linux"
+      ];
+
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f { pkgs = (pkgsFor system); });
+
+      pkgsFor = system: import nixpkgs { inherit system; };
+
+    in
+    {
+
+      devShells = forAllSystems (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.cabal-install
+              pkgs.haskell.compiler.${ghc}
+              pkgs.haskell.packages.${ghc}.cabal-gild
+              pkgs.haskell.packages.${ghc}.haskell-language-server
+              pkgs.hlint
+              pkgs.fourmolu
+            ];
+          };
+        }
+      );
+    };
+}
